534a5be168f700df5af652c2ca17b329c835491d
tonia <tonia@cia.como>
2022-06-14 15:40:05 +0200

    fine compito

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -1,9 +1,19 @@
 package jana60.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*una classe Main contenente il metodo main, nel quale chiedete all’utente di inserire, da console, i valori di base e di altezza con cui istanziare un nuovo oggetto Rettangolo. Dopo averlo istanziato, stampate a video il 
+		perimetro e l’area.*/ 
+
+		Rettangolo rett = new Rettangolo();
+		rett.altezza = 20;
+		rett.base = 10;
+		
+		System.out.println("L'area del rettangolo è: " + rett.calcolaArea());
+		System.out.println("Il perimetro del rettangolo è: " + rett.calcolaPerimetro());
 
 	}
 
--- src/jana60/geometria/Rettangolo.java
+++ src/jana60/geometria/Rettangolo.java
@@ -1,5 +1,25 @@
 package jana60.geometria;
 
 public class Rettangolo {
-
+ /*Nel progetto java-oop-geometria creare un package jana60.geometria in cui inserire le seguenti classi:
+una classe Rettangolo con due attributi interi: base e altezza. La classe ha due metodi che calcolano 
+e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+*/
+	
+	//ATTRIBUTI
+	int base;
+	int altezza;
+	
+	
+	//METODI
+	int calcolaArea() {
+		int area = base * altezza;
+		return area;
+	}
+	int calcolaPerimetro() {
+		int perimetro = (base + altezza) * 2;
+		return perimetro;
+	}
+	
+	
 }
